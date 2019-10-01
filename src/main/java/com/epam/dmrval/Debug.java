0cf9d50591d451a7aba249e0026158646f743405
Валеев Дамир Ринатович <dmrval@ibida.ru>
2019-10-01 15:56:35 +0400

    delDegug

--- src/main/java/com/epam/dmrval/Debug.java
+++
@@ -1,9 +0,0 @@
-package com.epam.dmrval;
-
-import com.epam.dmrval.entity.UsersHelper;
-
-/** Author - Damir_Valeev Created on 9/28/2019 */
-public class Debug {
-  public static void main(String[] args) {
-  }
-}
